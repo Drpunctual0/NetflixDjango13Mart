9adbc4eb33875a6f38bde26761fe8c59c1157e6d
Drpunctual0 <topciruveyda@gmail.com>
2023-06-16 21:28:39 +0300

    debug false

--- netflix/settings.py
+++ netflix/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%+*&o!hj06)8ehbk1*rs_=()lzpfyky0$&zg!6txwj4$rju9_s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,16 +122,21 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+if DEBUG == True:
+    STATIC_URL = 'static/'
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static'
+    ]
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
-
-MEDIA_URL = ''
-MEDIA_ROOT = os.path.join(
-    BASE_DIR / 'media'
-)
+    MEDIA_URL = ''
+    MEDIA_ROOT = os.path.join(
+        BASE_DIR / 'media'
+    )
+else:
+    STATIC_URL = 'static/'
+    STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
+    MEDIA_URL = 'media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
